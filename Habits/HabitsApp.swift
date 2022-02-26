57f92e19a1ddc7b4ca148372a8e17e704091a2f8
Davi Aquila <daviaquila162@gmail.com>
2022-02-26 15:42:39 -0500

    Init project

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Davi Aquila on 2022-02-26.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
